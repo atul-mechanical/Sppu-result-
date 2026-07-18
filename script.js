4982e065851f777bea6b32b76d23b4a7f7f5f862
atul-mechanical <8pv7vcjsv6@privaterelay.appleid.com>
2026-07-18 13:14:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-function checkResult() {
+function checkResult(){
 
-    let prn = document.getElementById("prn").value.trim().toUpperCase();
+let prn = document.getElementById("prn").value.trim().toUpperCase();
 
-    if(prn === "72240563E"){
-        document.getElementById("result").style.display="block";
-    }else{
-        alert("Invalid PRN / Application Number");
-        document.getElementById("result").style.display="none";
-    }
+if(prn === "72240563E"){
+
+document.getElementById("result").style.display="block";
+
+studentName.innerHTML="ATUL BAPU GANGURDE";
+studentPRN.innerHTML="72240563E";
+seatNo.innerHTML="B400410087";
+college.innerHTML="Brahma Valley College of Engineering";
+course.innerHTML="Mechanical Engineering";
+semester.innerHTML="Semester VI";
+sgpa.innerHTML="9.40";
+cgpa.innerHTML="8.90";
+
+}
+
+else{
+alert("Invalid PRN / Application Number");
+document.getElementById("result").style.display="none";
+}
 
 }

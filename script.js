f37460f6f53ba64397f5ce68d5db4a96f026c2f1
atul-mechanical <8pv7vcjsv6@privaterelay.appleid.com>
2026-07-18 13:41:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 function checkResult(){
 
-let prn = document.getElementById("prn").value.trim().toUpperCase();
+    let prn = document.getElementById("prn").value.trim().toUpperCase();
 
-if(prn === "72240563E"){
+    if(prn === "72240563E"){
 
-document.getElementById("result").style.display="block";
+        document.getElementById("result").style.display="block";
 
-studentName.innerHTML="ATUL BAPU GANGURDE";
-studentPRN.innerHTML="72240563E";
-seatNo.innerHTML="B400410087";
-college.innerHTML="Brahma Valley College of Engineering";
-course.innerHTML="
-semester.innerHTML="Semester VII & Semester VIII";
-sgpa.innerHTML="9.40";
-cgpa.innerHTML="8.90";
+        document.getElementById("studentName").innerHTML="ATUL BAPU GANGURDE";
+        document.getElementById("studentPRN").innerHTML="72240563E";
+        document.getElementById("seatNo").innerHTML="B400410087";
+        document.getElementById("college").innerHTML="Brahma Valley College of Engineering";
+        document.getElementById("course").innerHTML="Mechanical Engineering";
+        document.getElementById("semester").innerHTML="Semester VII & Semester VIII";
+        document.getElementById("sgpa").innerHTML="9.40";
+        document.getElementById("cgpa").innerHTML="8.90";
 
-}
+    }
 
-else{
-alert("Invalid PRN / Application Number");
-document.getElementById("result").style.display="none";
-}
+    else{
+
+        alert("Invalid PRN / Application Number");
+        document.getElementById("result").style.display="none";
+
+    }
 
 }
